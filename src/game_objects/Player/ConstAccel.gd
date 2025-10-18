124f252000626d5fd70fcaa512e54182567fcf6b
Darr3n <darrensoonyuchien@gmail.com>
2025-10-18 16:35:37 +0800

    added const accel

--- src/game_objects/Player/ConstAccel.gd
+++ src/game_objects/Player/ConstAccel.gd
@@ -4,17 +4,24 @@ class_name ConstAccel extends AccelStrategy
 @export var decel_time: float = 0.1
 @export var turn_time: float = 0.2
 
+var final_time: float
+
 
 func apply_acceleration(movement: PlayerMovement, direction: float, delta: float) -> void:
-	var final_time := get_final_time(movement, direction)
+	final_time = get_final_time(movement, direction)
 	
 	var ticks := delta / final_time
 	movement.velocity_x = move_toward(movement.velocity_x, movement.max_speed * direction, movement.max_speed * ticks)
 	
-func get_final_time(movement: PlayerMovement, direction: float) -> float:
-	var final_time = decel_time
+func get_final_time(movement: PlayerMovement, direction: float) -> float:	
+	if movement.lr_input.just_pressed():
+		var is_turning = movement.velocity_x != 0.0 and sign(movement.velocity_x) != direction
+		if is_turning:
+			return turn_time
+		else:
+			return accel_time
 	
-	if direction:
-		final_time = accel_time
-		
+	elif movement.lr_input.just_released():
+		return decel_time
+	
 	return final_time
