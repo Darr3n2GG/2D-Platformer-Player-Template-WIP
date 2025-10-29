fb9b0de4951049a9ca2d4d700453a530a0f492cd
Darr3n <darrensoonyuchien@gmail.com>
2025-10-29 22:31:52 +0800

    added handle vertical movement function

--- src/game_objects/Player/player.gd
+++ src/game_objects/Player/player.gd
@@ -9,21 +9,22 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
-	handle_movement_input(delta)
+	handle_horizontal_movement(delta)
+	handle_vertical_movement(delta)
+	move_and_slide()
 	
+func handle_horizontal_movement(delta: float) -> void:
+	if not movement_enabled:
+		return
+		
+	accel_strategy.apply_acceleration(self, get_direction(), delta)
+	
+func handle_vertical_movement(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -500
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	move_and_slide()
-	
-func handle_movement_input(delta: float) -> void:
-	if not movement_enabled:
-		return
-		
-	accel_strategy.apply_acceleration(self, get_direction(), delta)
 	
 	
 func get_direction() -> float:
